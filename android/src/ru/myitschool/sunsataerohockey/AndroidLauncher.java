ca464b2a7e5b0a6cc62a3f82b5aadcb5d569552a
teacher <tibedox@mail.ru>
2023-04-23 16:07:33 +0600

    fullscreen

--- android/src/ru/myitschool/sunsataerohockey/AndroidLauncher.java
+++ android/src/ru/myitschool/sunsataerohockey/AndroidLauncher.java
@@ -14,11 +14,11 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGame(), config);
 		View rootView = AndroidLauncher.this.getWindow().getDecorView().findViewById(android.R.id.content);
-		// View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-		// View.SYSTEM_UI_FLAG_FULLSCREEN |
-		// View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-		// View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-		// View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+		// View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+		// View.SYSTEM_UI_FLAG_FULLSCREEN
+		// View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		// View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+		// View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 		// View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 		rootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
